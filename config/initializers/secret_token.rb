6155356086ba0bb1e49be560eda8c2b1b37cd2dc
sharathkumar <sharathkumar@qburst.com>
2013-10-08 11:10:01 +0530

    PMtool First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InvokePmTool::Application.config.secret_key_base = 'c02e4141c5abd0c4585f9a9e679c28f7da17153d24447603661e3da4f623eb9902f86ec09352e4ae327b7b7bea623bc59b9009175941c528cb1d5f7d8ac9f328'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InvokePmTool::Application.config.session_store :cookie_store, key: '_Invoke_pm_tool_session'
